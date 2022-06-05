b4df1b88f312440d6962d618baf7d009d98d00b7
Frank-masciopinto <francesco.masciopinto@outlook.com>
2022-06-05 09:53:10 +0700

    Added Comc Scraper

--- script/content.js
+++ script/content.js
@@ -1,4 +1,4 @@
-console.log("HUYP --- Content.js is running!")
+console.log("COMCY --- Content.js is injected!")
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
   console.log(request)
@@ -7,27 +7,20 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
       toggle();
       sendResponse({mess: "DONE"})
   }
-  else if(request.message === "Close_IFrame"){
-      iframe.style.width="0px";
-      sendResponse({mess: "DONE"})
-  }
 })
 
+let all_Cards_Array = []
 
-var iframe = document.createElement('iframe'); 
-iframe.style.height = "100%";
-iframe.style.width = "0px";
-iframe.style.position = "fixed";
-iframe.classList.add("huyp-frame")
-iframe.style.top = "0px";
-iframe.style.right = "0px";
-iframe.style.zIndex = "9000000000000000000";
-iframe.style.border = "1px solid rgb(0, 122, 236)"; 
-iframe.style.borderRadius = "50px";
-iframe.src = chrome.runtime.getURL("html/slider-search.html")
-
-function toggle(){
-  document.body.appendChild(iframe);
-  iframe.style.width="400px";
-
+//Test If Browsing a Collection
+document.getElementById("cardexplorer")
+//Get All Cards
+let all_Cards = document.querySelectorAll(".cardInfoWrapper")
+//Collecting All Cards Info and adding it to Array
+for (let i = 0; i < all_Cards.length; i++) {
+  let card_Info = {}
+  card_Info.index = i
+  card_Info.title = all_Cards[i].querySelector(".title").innerText
+  card_Info.description = all_Cards[i].querySelector(".description").innerText
+  card_Info.list_Price = all_Cards[i].querySelector(".listPrice").innerText
+  all_Cards_Array.push(card_Info)
 }
